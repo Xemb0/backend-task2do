e071a6568ad785e09043ed4903aca381e003ec21
Xembo <nishadvishal937@gmail.com>
2024-06-23 13:30:58 -0700

    task

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Task = require('../models/task');
+const Task = require('../models/task.js');
 
 // Fetch all tasks
 router.get('/', async (req, res) => {
